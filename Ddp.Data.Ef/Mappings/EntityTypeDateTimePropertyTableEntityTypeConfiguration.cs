d452c8f59827a59ca944e499d5bb5cb237ed8a1b
krausep <krausep@gmail.com>
2019-03-06 19:35:33 -0600

    Fixed the tables and mapping configuration for two entity type properties (#2)

--- Ddp.Data.Ef/Mappings/EntityTypeDateTimePropertyTableEntityTypeConfiguration.cs
+++ Ddp.Data.Ef/Mappings/EntityTypeDateTimePropertyTableEntityTypeConfiguration.cs
@@ -9,7 +9,10 @@ namespace Ddp.Data.Ef.Mappings
         public void Configure(EntityTypeBuilder<EntityTypeDateTimePropertyTable> builder)
         {
             builder.HasKey(x => x.EntityTypeDateTimePropertyId);
-            builder.Property(x => x.DateTimeProperty).IsRequired();
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.MinimumValue);
+            builder.Property(x => x.MaximumValue);
+            builder.Property(x => x.IsRequired).IsRequired();
             builder.Property(x => x.MaximumAllowed);
 
             builder.HasOne<EntityTypeTable>().WithMany().HasForeignKey(x => x.EntityTypeId);
